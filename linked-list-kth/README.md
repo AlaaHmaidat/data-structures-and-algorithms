718a084614da7fd81939651a0d68906e1fa1defc
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-04-29 08:02:01 +0000

    solve kthFromEnd Code Challenge

---
+++ linked-list-kth/README.md
@@ -0,0 +1,40 @@
+# Linked List Kth
+
+## Whiteboard Process
+![WhiteboardWorkflow01](../img/Whiteboard%20Linked%20List%20Kth.jpg)
+
+## Approach & Efficiency
+
+1. __init__(self): Initializes an empty linked list with a None value for head.
+
+2. append(self, new_value): Inserts a new node with the given value at the end of the linked list.
+
+    * Create a new node with the new_value.
+
+    * If the linked list is empty (i.e., head is None), set head to the new node.
+    * Otherwise, traverse the linked list until the end (temp.next is None) and set the next pointer of the last node to the new node.
+
+3. kthFromEnd(self, k): Searches in the linked list for a node with the given index (k)
+    * Create a temp which equal self.head.
+    * Create a length variable which equal zero to calculate the length of the linked list
+
+    * While temp not none increase length by 1 and make temp equal temp.next
+
+    * Create a current which equal self.head.
+    * Create a count which equal length-1 because we need start from zero.
+
+    * While current not none
+    * check if k less than 0 or grater than the length return Exception
+    * Otherwise, if the count equal k return the value (current.value)
+    * Otherwise, decrease the count by 1 and make current equal current.next and continue looping
+    
+### Big O
+__init__: O(1)
+append: O(n), where n is the number of nodes in the linked list, because in the worst case we need to traverse the entire linked list to find the last node to append the new node.
+
+kthFromEnd: O(n), where n is the number of nodes in the linked list, because in the worst case we need to traverse the entire linked list to find the value of the node.
+
+The space complexity of this implementation is O(n), where n is the number of nodes in the linked list, because we need to store all of the nodes in memory.
+## Solution
+
+Click [here](./linked-list-kth.py)
---
+++ linked-list-kth/linked-list-kth.py
@@ -0,0 +1,88 @@
+
+class Node:
+    """
+    A Node object represents a single element in a linked list.
+
+    Args:
+        value: The value stored in the node.
+        next: A pointer to the next node in the sequence. Initially set to None.
+    """
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Linked_List_Kth:
+    """
+    A Linked_List_Kth object represents a node value in linked list using index.
+
+    Attributes:
+        head: A pointer to the first node in the linked list. Initially set to None.
+    """
+
+    def __init__(self):
+        """
+        Initializes an empty linked list with a None value for head.
+        """
+        self.head = None
+
+    def append(self, new_value):
+        """
+        Inserts a new node with the given value at the end of the linked list.
+
+        Args:
+            new_value: The value to be inserted into the linked list.
+
+        Returns:
+            A string indicating that the value has been successfully inserted.
+        """
+        node = Node(new_value)
+
+        if self.head is None:
+            self.head = node
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = node
+        return f"insert {new_value} successfully"
+
+    def kthFromEnd(self, k):
+        """
+        Searches in the linked list for a node with the given index.       
+
+        Args:
+            k: The index to searched in linked list.
+
+        Returns:
+            A value of node was found in the linked list using index.
+        """
+        temp = self.head
+        length = 0
+
+        while (temp):
+            length += 1  # Calculate the length of the linked list
+            temp = temp.next
+
+        current = self.head  # Initialise temp
+        count = length-1  # Index of last node
+
+        while (current):
+            if (k < 0 or k > length):
+                return "Exception"
+            elif (count == k):
+                return current.value
+            elif (count > k):
+                count -= 1
+                current = current.next
+                continue
+
+
+if __name__ == '__main__':
+    ll = Linked_List_Kth()
+    ll.append(1)  # index: 3
+    ll.append(3)  # index: 2
+    ll.append(8)  # index: 1
+    ll.append(2)  # index: 0
+
+    print(ll.kthFromEnd(3))
