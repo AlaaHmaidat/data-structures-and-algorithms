7a12b4477eafe358f4855e0da3b719543736aee4
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-05-10 19:22:14 +0000

    created First-in, First out Animal Shelter

---
+++ linked-list-zip/test_linked_list_zip.py
@@ -0,0 +1,11 @@
+import pytest
+from linked_list_zip import Linked_List_Zip
+
+# Test case 1
+# Input:
+# head1: 1 -> 3 -> 5 -> None
+# head2: 2 -> 4 -> None
+# Output:
+# 1 -> 2 -> 3 -> 4 -> 5 -> None
+def test_zip_linked_list():
+    pass
