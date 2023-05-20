7a0bf157d7c8166a8c77ae0a9a0287e7057d6135
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-05-20 19:10:05 +0000

    solve trees code challenge

--- trees/trees.py
+++ trees/trees.py
@@ -1,4 +1,11 @@
-from Node import Node
+class Node:
+    '''
+    Node class that has properties for the value stored in the node, the left child node, and the right child node
+    '''
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
 class binary_tree:
     '''
     Binary Tree class
@@ -13,28 +20,54 @@ class binary_tree:
     def __init__(self):
         self.root = None
 
-    def pre_order(self,root,list1=[]):# root,left,right (the result shoud be in this ex = L T C A U S A)
-        
+    def pre_order(self,root):# root,left,right 
+        '''
+        Perform pre-order traversal on the binary tree.
+
+        Arguments:
+        - root: The root node of the binary tree.
+
+        Returns:
+        - A list of values obtained through pre-order traversal.
+        '''
+        list1=[]
         if root is not None:# root
             list1.append(root.value)
-        if root.left is not None:# left
-            self.pre_order(root.left,list1)
-        if root.right is not None:# right
-            self.pre_order(root.right,list1)
+            list1.extend(self.pre_order(root.left)) # used to append the elements returned by the recursive calls to the traversal methods
+            list1.extend(self.pre_order(root.right))
 
         return list1
 
-    def in_order(self,root,list1=[]):# left,root,right (the result shoud be in this ex = C T A L S U A)
+    def in_order(self,root):# left,root,right 
+        '''
+        Perform in-order traversal on the binary tree.
+
+        Arguments:
+        - root: The root node of the binary tree.
+
+        Returns:
+        - A list of values obtained through in-order traversal.
+        '''
+        list1=[]
         if root is not None:# root
-            self.in_order(root.left)
+            list1.extend(self.in_order(root.left))
             list1.append(root.value)
-            self.in_order(root.right) 
+            list1.extend(self.in_order(root.right)) 
         return list1
-    def post_order(self,root,list1=[]):# left,right,root (the result shoud be in this ex = C A T S A U L)
-        
+    def post_order(self,root):# left,right,root 
+        '''
+        Perform post-order traversal on the binary tree.
+
+        Arguments:
+        - root: The root node of the binary tree.
+
+        Returns:
+        - A list of values obtained through post-order traversal.
+        '''
+        list1=[]
         if root is not None:# root
-            self.post_order(root.left)
-            self.post_order(root.right) 
+            list1.extend(self.post_order(root.left))
+            list1.extend(self.post_order(root.right)) 
             list1.append(root.value)
         return list1
 
@@ -45,54 +78,96 @@ class binary_search_tree(binary_tree):
     def __init__(self):
         binary_tree.__init__(self)
 
-    def Add(value):
+    def Add(self,value):
         '''
         Arguments: value
         Return: nothing
         Adds a new node with that value in the correct location in the binary search tree.
         '''
-        pass
+        if self.root is None:# edge cases
+             self.root = Node(value)
+             
+        self._add(self.root,value)
+    
+    def _add(self,root,value):# helper method
+        '''
+        helper method
+        Arguments: value,root
+        Return: nothing
+        Adds a new node with that value in the correct location in the binary search tree.
+        '''
+        if value < root.value:
+            if root.left is None:
+                root.left = Node(value)
+            else:
+                self._add(root.left, value)
+        elif value > root.value:
+            if root.right is None:
+                root.right = Node(value)
+            else:
+                self._add(root.right, value)
+               
 
     def Contains(self,value):
         '''
         Argument: value
         Returns: boolean indicating whether or not the value is in the tree at least once.
         '''
-        print(self.root.value)
         return self._contains(self.root,value)
     
     def _contains(self,root,value):# helper method
-        print(root.value)
         '''
-        Argument: value
+        helper method
+        Argument: value,root
         Returns: boolean indicating whether or not the value is in the tree at least once.
         '''
-        if root is None:# edge casses
+        if root is None:# edge cases
             return False
-
         elif root.value == value:
-            return True 
+            return True
         
+        # if type(value) == str:
+            # if ord(root.value) < ord(value):
+        
+        if value <  root.value:
+            if root.left is not None:
+                if root.left.value == value:
+                        return True
+                else:
+                        if self._contains(root.left,value):
+                            return True
+                    
+        if value >  root.value:
+            if root.right is not None:
+                    if root.right.value == value:
+                        return True
+                    else:
+                        if self._contains(root.right,value):
+                            return True
+
+        return False        
+                    
 
 if __name__=='__main__':
 
     '''
         L
        / \
-      T   U
-     / \ / \
-    C  A S  A
+      C   T
+     /   / \
+    A   S   U
 
     '''
-
     tree1 = binary_search_tree()
+    tree1.Add("L")
+    tree1.Add("T")
+    tree1.Add("U")
+    tree1.Add("C")
+    tree1.Add("A")
+    tree1.Add("S") 
+    
+    print("Pre-order traversal:", tree1.pre_order(tree1.root)) # ['L', 'C', 'A', 'T', 'S', 'U']
+    print("In-order traversal:", tree1.in_order(tree1.root)) # ['A', 'C', 'L', 'S', 'T', 'U']
+    print("Post-order traversal:", tree1.post_order(tree1.root)) # ['A', 'C', 'S', 'U', 'T', 'L']
 
-    tree1.root = Node("L")
-    tree1.root.left = Node("T")
-    tree1.root.right = Node("U")
-    tree1.root.left.left = Node("C")
-    tree1.root.left.right = Node("A")
-    tree1.root.right.left = Node("S")
-    tree1.root.right.right = Node("A")
-
-    print(tree1.Contains('L'))
+    print(tree1.Contains('C'))
