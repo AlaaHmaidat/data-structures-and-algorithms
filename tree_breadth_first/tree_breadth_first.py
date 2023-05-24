4790891e9468ace9780e2e54040974c41c47190e
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-05-24 18:52:55 +0000

    Tree Breadth First Search function

---
+++ tree_breadth_first/tree_breadth_first.py
@@ -0,0 +1,7 @@
+def tree_breadth_first():
+    '''
+    Tree Breadth First Search function
+    Arguments: tree
+    Return: list of all values in the tree, in the order they were encountered
+    '''
+    pass
